39658abe08dbad8832038629ff465011cbe1a632
GoatYogurt <minhquan161004@gmail.com>
2023-02-08 18:25:04 +0700

    Create ex16.cpp

---
+++ partB/ex16.cpp
@@ -0,0 +1,6 @@
+//Viết một chương trình đọc ba số nguyên từ bàn phím và in ra "true" nếu cả ba số bằng nhau, nếu không thì in ra "false".
+#include <iostream>
+using namespace std;
+int main() {
+    
+}
