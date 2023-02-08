da3da2bb1c07963636b5ca957085853f759b9b9a
GoatYogurt <minhquan161004@gmail.com>
2023-02-08 18:40:26 +0700

    Update ex17.cpp

--- partB/ex17.cpp
+++ partB/ex17.cpp
@@ -3,5 +3,12 @@
 using namespace std;
 int main() {
 	int year; cin >> year;
-	if (
+	if (year % 4 != 0)
+        cout << "false";
+    else {
+        if (year % 100 == 0)
+            if (year % 400 == 0)
+                cout << "true";
+            else cout >> "fasle";
+    }
 }
