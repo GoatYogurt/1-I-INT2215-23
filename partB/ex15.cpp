edf2c6c01e82dc043090da7a436f18af56427b25
GoatYogurt <minhquan161004@gmail.com>
2023-02-08 18:21:23 +0700

    Add files via upload

---
+++ partB/ex15.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    string f0 = "a", f1 = "b", fn = "bab";
+    cout << f0 << " " << f1 << " " << fn;
+    for (int i = 3; i <= 10; i++) {
+        f0 = f1;
+        f1 = fn;
+        fn = f1 + f0;
+        cout <<" " << fn;
+    }
+}
