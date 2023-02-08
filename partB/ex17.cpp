f7983e9cc4b50003e2061582555d6f5766dc3970
GoatYogurt <minhquan161004@gmail.com>
2023-02-08 18:40:02 +0700

    Create ex17.cpp

---
+++ partB/ex17.cpp
@@ -0,0 +1,7 @@
+//Viết chương trình LeapYear kiểm tra năm nhuận. Chương trình đọc vào một số nguyên từ bàn phím và in ra "true" nếu đó là một năm nhuận, nếu không thì in ra "false"
+#include <iostream>
+using namespace std;
+int main() {
+	int year; cin >> year;
+	if (
+}
