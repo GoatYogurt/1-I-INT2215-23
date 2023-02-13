734cb63ef98a94b0f1c6d688cc9d76970f7e496c
GoatYogurt <minhquan161004@gmail.com>
2023-02-13 22:01:11 +0700

    Update ex17.cpp

--- BTLTNC/Week1/partB&C/ex17.cpp
+++ BTLTNC/Week1/partB&C/ex17.cpp
@@ -1,14 +1,20 @@
-//Viết chương trình LeapYear kiểm tra năm nhuận. Chương trình đọc vào một số nguyên từ bàn phím và in ra "true" nếu đó là một năm nhuận, nếu không thì in ra "false"
 #include <iostream>
 using namespace std;
 int main() {
-	int year; cin >> year;
-	if (year % 4 != 0)
-        cout << "false";
-    else {
-        if (year % 100 == 0)
-            if (year % 400 == 0)
-                cout << "true";
-            else cout >> "fasle";
+    int year = 1;
+    while (year != 0) {
+        cin >> year;
+        if (year % 4 != 0) {
+            cout << "false" << endl;
+        }
+
+        else {
+            if (year % 100 == 0)
+                if (year % 400 == 0)
+                    cout << "true";
+                else cout << "fasle";
+            else cout << "true";
+        cout << endl;
+        }
     }
 }
