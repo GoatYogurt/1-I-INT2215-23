e64ceb2e2fcc5994135ad06c6bc5c06aec336b2e
GoatYogurt <minhquan161004@gmail.com>
2023-04-13 17:39:55 +0700

    lam ex7 week 5

--- BTLTNC/BT05/partC/ex7.cpp
+++ BTLTNC/BT05/partC/ex7.cpp
@@ -9,8 +9,12 @@ void print(int m, int n) {
     for (int i = 1; i <= n; ++i) {
         cout << "*";
     }
+    cout << endl;
 }
 
 int main() {
-    
+    int n; cin >> n;
+    for (int i = n - 1; i >= 0; --i) {
+        print(i, 2 * (n - i) - 1);
+    }
 }
